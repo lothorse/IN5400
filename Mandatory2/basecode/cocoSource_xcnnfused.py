d1da9574c02b373f86dad6311947ff353b4345e0
lothorse <lothorse@ifi.uio.no>
2021-05-11 14:05:43 +0200

    changed cat order

--- Mandatory2/basecode/cocoSource_xcnnfused.py
+++ Mandatory2/basecode/cocoSource_xcnnfused.py
@@ -137,7 +137,7 @@ class RNN_onelayer_simplified(nn.Module):
             # and input at each layer the correct input ,
             # the input at higher layers will be the hidden state from the layer below
             #TODO
-            lvl0input = torch.cat((baseimgfeat, xTokens), 1) # what
+            lvl0input = torch.cat((xTokens, baseimgfeat), 1) # what
             lvl0input = torch.unsqueeze(lvl0input.cpu(), 0)
             print(lvl0input.shape)
             #note that      current_state has 3 dims ( ...len(current_state.shape)==3... ) with first dimension having only 1 element, while the rnn cell needs a state with 2 dims as input
