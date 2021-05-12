fc115cfacbf5a6de9239b2e5537eb87ade7f92a9
lothorse <lothorse@ifi.uio.no>
2021-05-12 12:55:50 +0200

    removed print

--- Mandatory2/basecode/cocoSource_xcnnfused.py
+++ Mandatory2/basecode/cocoSource_xcnnfused.py
@@ -139,7 +139,6 @@ class RNN_onelayer_simplified(nn.Module):
             #TODO
 
             lvl0input = torch.cat((tokens_vector, baseimgfeat), 1) # what
-            print(lvl0input.shape)
             #note that      current_state has 3 dims ( ...len(current_state.shape)==3... ) with first dimension having only 1 element, while the rnn cell needs a state with 2 dims as input
             #TODO
             updatedstate[0,:] = self.cells[0](lvl0input, torch.squeeze(current_state))[0,:]  #RNN cell is used here #uses lvl0input and the hiddenstate
