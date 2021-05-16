8a610a63586221aac8be26693310df8a9d9c8b35
lothorse <lothorse@ifi.uio.no>
2021-05-16 22:06:28 +0200

    m

--- Mandatory2/basecode/cocoSource_xcnnfused.py
+++ Mandatory2/basecode/cocoSource_xcnnfused.py
@@ -347,9 +347,9 @@ class RNNsimpleCell(nn.Module):
 
         """
         x2 = torch.cat((x, state_old), dim=1)
-        print(self.hidden_state_size)
-        print(self.weight.shape)
-        print(state_old.shape, x2.shape, torch.mm(x2, self.weight).shape, (torch.mm(x2, self.weight) + self.bias).shape)
+        #print(self.hidden_state_size)
+        #print(self.weight.shape)
+        #print(state_old.shape, x2.shape, torch.mm(x2, self.weight).shape, (torch.mm(x2, self.weight) + self.bias).shape)
         state_new = torch.tanh(torch.mm(x2, self.weight) + self.bias)
         return state_new
 
