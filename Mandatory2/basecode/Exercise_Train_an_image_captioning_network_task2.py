0215bcb8942d4faef4c6d06e0739e9c87476a2a4
lothorse <lothorse@ifi.uio.no>
2021-05-16 18:07:27 +0200

    lowered number of epochs

--- Mandatory2/basecode/Exercise_Train_an_image_captioning_network_task2.py
+++ Mandatory2/basecode/Exercise_Train_an_image_captioning_network_task2.py
@@ -44,7 +44,7 @@ if __name__ == '__main__':
         'cuda': {'use_cuda': True,  # Use_cuda=True: use GPU
                  'device_idx': 0},  # Select gpu index: 0,1,2,3
         'numbOfCPUThreadsUsed': 10,  # Number of cpu threads use in the dataloader
-        'numbOfEpochs': 99,  # Number of epochs
+        'numbOfEpochs': 35,  # Number of epochs
         'data_dir': data_dir,  # data directory
         'img_dir': 'loss_images_test/',
         'modelsDir': 'storedModels_test/',
